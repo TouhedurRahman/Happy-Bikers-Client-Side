15606d22157f90ca84fb46ff8202e118c83f438c
TouhedurRahman <touhedur.cse@gmail.com>
2023-01-21 18:44:26 +0600

    site banner added

---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Carousel, Container } from 'react-bootstrap';
+
+const Banner = () => {
+    return (
+        <div>
+            <Container fluid m-3>
+                <Carousel variant="dark">
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src="../../../images/banner.jpg"
+                            alt="First slide"
+                        />
+                        <Carousel.Caption>
+                            <h5>First slide label</h5>
+                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src="../../../images/banner.jpg"
+                            alt="Second slide"
+                        />
+                        <Carousel.Caption>
+                            <h5>Second slide label</h5>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src="../../../images/banner.jpg"
+                            alt="Third slide"
+                        />
+                        <Carousel.Caption>
+                            <h5>Third slide label</h5>
+                            <p>
+                                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                            </p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                </Carousel>
+            </Container>
+        </div>
+    );
+};
+
+export default Banner;
