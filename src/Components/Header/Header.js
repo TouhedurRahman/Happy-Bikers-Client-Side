59bfca47dc43f2dbc27afe85c41274608fe6bacf
TouhedurRahman <touhedur.cse@gmail.com>
2023-01-21 18:21:54 +0600

    site navbar added

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import "./Header.css";
+
+const Header = () => {
+    return (
+        <div>
+            <Container fluid>
+                <div className="nav-section">
+                    <Navbar collapseOnSelect expand="lg">
+                        <Container>
+                            <img width='50px' height='30%' className='me-1' src="../../../images/site-logo/bike-shop-logo.png" alt="" />
+                            <Navbar.Brand href="#home">Happy Bikers</Navbar.Brand>
+
+                            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                            <Navbar.Collapse id="responsive-navbar-nav">
+                                <Nav className="me-auto">
+                                    <Nav.Link href="./home">Home</Nav.Link>
+                                    <Nav.Link href="./services">Services</Nav.Link>
+                                    <Nav.Link href="./blog">Blog</Nav.Link>
+                                    <Nav.Link href="./about">About Us</Nav.Link>
+                                </Nav>
+                                <Nav>
+                                    <Nav.Link href="#deets">More</Nav.Link>
+                                    <Nav.Link href="#deets">Login</Nav.Link>
+                                </Nav>
+                            </Navbar.Collapse>
+                        </Container>
+                    </Navbar>
+                </div>
+            </Container>
+        </div>
+    );
+};
+
+export default Header;
