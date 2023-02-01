87828225a783bd54e5ad36ea636cf3cb32cf7ce5
TouhedurRahman <touhedur.cse@gmail.com>
2023-02-01 22:26:29 +0600

    Bikes-Restock client site design added

--- src/Components/UpdateItem/UpdateItem.js
+++ src/Components/UpdateItem/UpdateItem.js
@@ -20,61 +20,91 @@ const UpdateItem = () => {
     }, [updateId]);
 
     return (
-        <div className='service-card'>
-            <div className='product-details'>
-                <Card className='border border-1 mx-auto m-1 card rounded-3' style={{ width: '20rem' }}>
-                    <Card.Img variant="top" width="250px" height="250px" src={product.image} />
-                    <Card.Body>
-                        <Card.Title>{product.title}</Card.Title>
-                        <Card.Text>
-                            {product.description}
-                        </Card.Text>
-                        <Card.Text>
-                            Current Price: {product.price}
-                        </Card.Text>
-                        <Card.Text>
-                            Available Stock: {product.quantity}
-                        </Card.Text>
-                        <Card.Text>
-                            Supplier Name: {product.supplierName}
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
-            </div>
-            <div className='product-delivery m-3'>
-                <h1 className='pt-3 text-center'>Bikes Delivery</h1>
-                <hr />
-                <Form>
-                    <Form.Group className="mb-3">
-                        <Form.Label>Bike Delivery</Form.Label>
-                        <Form.Control
-                            name="delevered"
-                            type="number"
-                            placeholder="Enter no. of quantity"
-                            required
-                        />
-                        <Form.Text className="text-muted">
-                            ** You have {product.quantity} bikes available
-                        </Form.Text>
-                    </Form.Group>
-                    <div className='text-center'>
-                        <Button variant="primary" type="submit">
-                            Bulk Delivery
-                        </Button>
-                    </div>
-                </Form>
-                <div className='d-flex align-items-center'>
-                    <div style={{ height: '1px' }} className='bg-primary w-50'></div>
-                    <p className='mt-2 px-2'>Or</p>
-                    <div style={{ height: '1px' }} className='bg-primary w-50'></div>
+        <div>
+            <div className='service-card'>
+                <div className='product-details'>
+                    <Card className='border border-1 mx-auto m-1 card rounded-3' style={{ width: '20rem' }}>
+                        <Card.Img variant="top" width="250px" height="250px" src={product.image} />
+                        <Card.Body>
+                            <Card.Title>{product.title}</Card.Title>
+                            <Card.Text>
+                                {product.description}
+                            </Card.Text>
+                            <Card.Text>
+                                Current Price: {product.price}
+                            </Card.Text>
+                            <Card.Text>
+                                Available Stock: {product.quantity}
+                            </Card.Text>
+                            <Card.Text>
+                                Supplier Name: {product.supplierName}
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
                 </div>
-                <Form>
-                    <div className='text-center'>
-                        <Button variant="primary" type="submit">
-                            Singal Delivery
-                        </Button>
+                <div className='product-delivery m-3'>
+                    <h1 className='pt-3 text-center'>Bikes Delivery</h1>
+                    <hr />
+                    <Form>
+                        <Form.Group className="mb-3">
+                            <Form.Label>Bikes Delivery</Form.Label>
+                            <Form.Control
+                                name="delevered"
+                                type="number"
+                                placeholder="Enter no. of quantity"
+                                required
+                            />
+                            <Form.Text className="text-muted">
+                                ** You have {product.quantity} bikes available
+                            </Form.Text>
+                        </Form.Group>
+                        <div className='text-center'>
+                            <Button variant="primary" type="submit">
+                                Bulk Delivery
+                            </Button>
+                        </div>
+                    </Form>
+                    <div className='d-flex align-items-center'>
+                        <div style={{ height: '1px' }} className='bg-primary w-50'></div>
+                        <p className='mt-2 px-2'>Or</p>
+                        <div style={{ height: '1px' }} className='bg-primary w-50'></div>
                     </div>
-                </Form>
+                    <Form>
+                        <div className='text-center'>
+                            <Button variant="primary" type="submit">
+                                Singal Delivery
+                            </Button>
+                        </div>
+                    </Form>
+                </div>
+                <div className='product-restock m-3'>
+                    <h1 className='pt-3 text-center'>Bikes Restock</h1>
+                    <hr />
+                    <Form>
+                        <Form.Group className="mb-3">
+                            <Form.Label>Bikes Restock</Form.Label>
+                            <Form.Control
+                                name="delevered"
+                                type="number"
+                                placeholder="Enter no. of quantity"
+                                required
+                            />
+                            <Form.Text className="text-muted">
+                                ** You have {product.quantity} bikes available
+                            </Form.Text>
+                        </Form.Group>
+                        <div className='text-center'>
+                            <Button variant="primary" type="submit">
+                                Restock
+                            </Button>
+                        </div>
+                    </Form>
+                </div>
+            </div>
+            <div className='text-center'>
+                <Button variant="primary" type="submit">
+                    Manage Stock
+                </Button>
             </div>
         </div>
     );
